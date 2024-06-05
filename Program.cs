3e21a8aff7bcd407a6dc98ce8cde9947b1523214
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-05 18:33:13 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double Seg, kilometros = 0;
+
+Console.WriteLine("Quantos segundos foi percorrido o tempo de trovão entre raio...: ");
+Seg = Convert.ToDouble(Console.ReadLine());
+
+void calculo(){
+    kilometros = Seg * 340.29;
+    kilometros = kilometros / 1000;
+}
+
+calculo();
+
+Console.WriteLine($"A distancia percorrida foi...: {kilometros}");
+
